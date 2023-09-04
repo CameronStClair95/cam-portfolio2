02ff1db7f61687bae105491414d840386e1d2892
CameronStClair95 <cameronaitcheson@gmail.com>
2023-09-04 17:54:31 +0100

    added a new project

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,44 +1,51 @@
-import falloutQuizImg from '../assets/images/falloutQuizImg.png'
+// import falloutQuizImg from '../assets/images/falloutQuizImg.png'
 import globeHopperImg from '../assets/images/globeHopperImg.png'
-import macArtsyImg from '../assets/images/macArtsyImg.png'
+// import macArtsyImg from '../assets/images/macArtsyImg.png'
 import Jordan_Alexander from '../assets/images/Jordan_Alexander.png'
 import OmniFood from '../assets/images/OmniFood.png'
+import Natours from '../assets/images/Natours.png'
 
 export const portfolio = [
+  // {
+  //   name: 'Fallout Game',
+  //   img: falloutQuizImg,
+  //   description: 'Basic JavaScript',
+  //   category: 'Games',
+  //   url: 'https://cameronstclair95.github.io/Fallout-Game-Ironhack-Project-1/',
+  //   github:
+  //     'https://github.com/CameronStClair95/Fallout-Game-Ironhack-Project-1',
+  // },
+
   {
-    name: 'Fallout Game',
-    img: falloutQuizImg,
-    description: 'Basic JavaScript',
-    url: 'https://cameronstclair95.github.io/Fallout-Game-Ironhack-Project-1/',
-    github:
-      'https://github.com/CameronStClair95/Fallout-Game-Ironhack-Project-1',
+    name: 'Natours',
+    img: Natours,
+    description: 'HTML, CSS, and JavaScript',
+    category: 'Web Design',
+    url: 'https://natours-cam.netlify.app/',
+    github: 'https://github.com/CameronStClair95/cam-natours',
   },
   {
     name: 'Globe Hopper',
     img: globeHopperImg,
     description: 'Node.js, Express, MongoDB, and EJS (Handle Bars)',
+    category: 'Web Application',
     url: 'https://globehopper.cyclic.app/',
     github: 'https://github.com/labarr-bell/project-2',
-  },
-  {
-    name: 'Macartsy',
-    img: macArtsyImg,
-    description: 'MERN stack',
-    url: 'https://macartsy.netlify.app/',
-    github: 'https://github.com/CameronStClair95/my-artsy-app-client',
-  },
-  {
-    name: 'Comissioned Website',
-    img: Jordan_Alexander,
-    description: 'HTML, CSS, and JavaScript',
-    url: 'https://jordanalexander.netlify.app/',
-    github: 'https://github.com/CameronStClair95/html-css-portfolio-jordan',
   },
   {
     name: 'OmniFood',
     img: OmniFood,
     description: 'HTML, CSS, and JavaScript',
+    category: 'Web Design',
     url: 'https://omnifood-cameron-a-l.netlify.app/',
     github: 'https://github.com/CameronStClair95/Omnifood',
+  },
+  {
+    name: 'Comissioned Website',
+    img: Jordan_Alexander,
+    description: 'HTML, CSS, and JavaScript',
+    category: 'Business Websites',
+    url: 'https://jordanalexander.netlify.app/',
+    github: 'https://github.com/CameronStClair95/html-css-portfolio-jordan',
   },
 ]
