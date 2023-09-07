133017bbeb10c3a0cd8a46eb27fe5010ab224c27
CameronStClair95 <cameronaitcheson@gmail.com>
2023-09-07 18:14:14 +0100

    Added Figma Project

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,26 +1,14 @@
-// import falloutQuizImg from '../assets/images/falloutQuizImg.png'
 import globeHopperImg from '../assets/images/globeHopperImg.png'
-// import macArtsyImg from '../assets/images/macArtsyImg.png'
 import Jordan_Alexander from '../assets/images/Jordan_Alexander.png'
 import OmniFood from '../assets/images/OmniFood.png'
 import Natours from '../assets/images/Natours.png'
+import KryptoImg from '../assets/images/KryptoImg.png'
 
 export const portfolio = [
-  // {
-  //   name: 'Fallout Game',
-  //   img: falloutQuizImg,
-  //   description: 'Basic JavaScript',
-  //   category: 'Games',
-  //   url: 'https://cameronstclair95.github.io/Fallout-Game-Ironhack-Project-1/',
-  //   github:
-  //     'https://github.com/CameronStClair95/Fallout-Game-Ironhack-Project-1',
-  // },
-
   {
     name: 'Natours',
     img: Natours,
     description: 'HTML, CSS, and JavaScript',
-    category: 'Web Design',
     url: 'https://natours-cam.netlify.app/',
     github: 'https://github.com/CameronStClair95/cam-natours',
   },
@@ -28,7 +16,6 @@ export const portfolio = [
     name: 'Globe Hopper',
     img: globeHopperImg,
     description: 'Node.js, Express, MongoDB, and EJS (Handle Bars)',
-    category: 'Web Application',
     url: 'https://globehopper.cyclic.app/',
     github: 'https://github.com/labarr-bell/project-2',
   },
@@ -36,7 +23,6 @@ export const portfolio = [
     name: 'OmniFood',
     img: OmniFood,
     description: 'HTML, CSS, and JavaScript',
-    category: 'Web Design',
     url: 'https://omnifood-cameron-a-l.netlify.app/',
     github: 'https://github.com/CameronStClair95/Omnifood',
   },
@@ -44,8 +30,13 @@ export const portfolio = [
     name: 'Comissioned Website',
     img: Jordan_Alexander,
     description: 'HTML, CSS, and JavaScript',
-    category: 'Business Websites',
     url: 'https://jordanalexander.netlify.app/',
     github: 'https://github.com/CameronStClair95/html-css-portfolio-jordan',
+  },
+  {
+    name: 'Krypto',
+    img: KryptoImg,
+    description: 'Figma',
+    url: 'https://www.figma.com/file/Czu51iyufdMIJsu2NKq0yk/Krypto-App-Idea?type=design&node-id=0%3A1&mode=design&t=VHe1Janckmbo1Xrj-1',
   },
 ]
