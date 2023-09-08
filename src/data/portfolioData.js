5f820f79a08a0da7ad5153e1bc2550744521c993
CameronStClair95 <cameronaitcheson@gmail.com>
2023-09-08 08:45:57 +0100

    Changed Description

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -41,7 +41,7 @@ export const portfolio = [
     url: 'https://www.figma.com/file/Czu51iyufdMIJsu2NKq0yk/Krypto-App-Idea?type=design&node-id=0%3A1&mode=design&t=VHe1Janckmbo1Xrj-1',
   },
   {
-    name: 'Portfolio Adaptation Idea',
+    name: 'Portfolio Adaptation Idea Work In Progress',
     img: Portfolioimg,
     description: 'Figma (Adaptation of this portfolio)',
     url: 'https://www.figma.com/file/g2M1cHZvc6HlA9gAzBmgNp/Cam-Portfolio-Upgrade-Idea?type=design&node-id=0%3A1&mode=design&t=QFfb3Vpq9I49lWEW-1',
