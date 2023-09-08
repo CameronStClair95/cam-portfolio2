e79c6f64ddfcca0c2836cc52bd659162112ba6b2
CameronStClair95 <cameronaitcheson@gmail.com>
2023-09-08 08:43:39 +0100

    added one more project

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -3,6 +3,7 @@ import Jordan_Alexander from '../assets/images/Jordan_Alexander.png'
 import OmniFood from '../assets/images/OmniFood.png'
 import Natours from '../assets/images/Natours.png'
 import KryptoImg from '../assets/images/KryptoImg.png'
+import Portfolioimg from '../assets/images/Portfolio-2-img.png'
 
 export const portfolio = [
   {
@@ -38,5 +39,11 @@ export const portfolio = [
     img: KryptoImg,
     description: 'Figma',
     url: 'https://www.figma.com/file/Czu51iyufdMIJsu2NKq0yk/Krypto-App-Idea?type=design&node-id=0%3A1&mode=design&t=VHe1Janckmbo1Xrj-1',
+  },
+  {
+    name: 'Portfolio Adaptation Idea',
+    img: Portfolioimg,
+    description: 'Figma (Adaptation of this portfolio)',
+    url: 'https://www.figma.com/file/g2M1cHZvc6HlA9gAzBmgNp/Cam-Portfolio-Upgrade-Idea?type=design&node-id=0%3A1&mode=design&t=QFfb3Vpq9I49lWEW-1',
   },
 ]
